002908615e622be8174d19725c3294dd623e5efd
chandan kumar <chandankumar070501@gmail.com>
2025-04-16 04:44:34 +0530

    origin set

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ const server = app.listen(port, () =>
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN_URL,
     credentials: true,
   },
 });
