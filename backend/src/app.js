4e41889f09386ace9be756f08f1c3844481b53ec
chandan kumar <chandankumar070501@gmail.com>
2025-04-16 14:11:35 +0530

    fix for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 app.use(
   cors({
-    origin: process.env.ORIGIN_URL,
+    origin: [process.env.ORIGIN_URL, "http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "OPTIONS", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
